1dcf7193ad4a19e44b4fb5d91037f8005f9a88eb
Gavin Mooney <2324864m@student.gla.ac.uk>
2019-01-14 21:40:13 +0000

    End of chapter 3 commit, forgot to include files

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from rango import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
+    # above maps any URLs starting
+    # with rango/ to be handled by
+    # the rango application
+    url(r'^rango/about', views.about, name='about'),
+    url(r'^rango/about', include('rango.urls')),
+    
     url(r'^admin/', admin.site.urls),
 ]
