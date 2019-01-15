f1e3376b9b5f6b6146f109a24e0b41db48d527e0
Gavin Mooney <2324864m@student.gla.ac.uk>
2019-01-15 20:10:55 +0000

    updated views to contain hyperlink instead of plain text

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!\nAbout: http://127.0.0.1:8000/rango/about")
+    return HttpResponse("Rango says \"hey there partner!\"<br><a href=http://127.0.0.1:8000/rango/about>About</a>")
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.\nIndex: http://127.0.0.1:8000/rango")
+    return HttpResponse("Rango says \"here is the about page.\"<br><a href=http://127.0.0.1:8000/rango>Index</a>")
